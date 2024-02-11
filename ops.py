42cfcd94d66a75836995d95936fd734157a56925
s7chak <s7chak@gmail.com>
2024-02-11 12:02:25 -0700

    analyzer on - email report

--- ops.py
+++ ops.py
@@ -67,10 +67,10 @@ def read_files(bucket_name, type_):
     relevant_files = []
     dfs = []
     for blob in blobs:
-        logging.info(blob.name)
+        print(blob.name)
         file_name = blob.name.split('/')[-1]
         file_date_str = file_name.split('.')[0]
-        logging.info(file_name)
+        print(file_name)
         try:
             file_date = dt.strptime(file_date_str, '%Y%m%d')
             if one_month_ago <= file_date <= current_date:
@@ -80,7 +80,7 @@ def read_files(bucket_name, type_):
                 dfs.append(df_)
         except:
             print('File read error: ', file_date_str, type_,str(sys.exc_info()))
-            logging.error('File read error: ' +file_date_str + type_ + '\n' +str(sys.exc_info()))
+            # logging.error('File read error: ' +file_date_str + type_ + '\n' +str(sys.exc_info()))
     df = pd.concat(dfs, axis=0) if len(dfs) else None
     return df, relevant_files
 
